1421952b0a02c8772284d878d03ad3f04c02d7a6
Romeo Van Snick <romeovs@gmail.com>
2016-01-18 15:52:56 +0100

    clean up run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+# set the ssl option
 if [ "$WERCKER_MD_MAIL_NOTIFY_SSL" = "yes" ]; then
   SSL="--ssl"
 fi
 
-# npm-install -g md-mailer
-md-mailer $SSL -h "$WERCKER_MD_MAIL_NOTIFY_HOST" -u "$WERCKER_MD_MAIL_NOTIFY_USER" "$WERCKER_MD_NOTIFY_FILE"
-
+md-mailer
+  $SSL \
+  -h "$WERCKER_MD_MAIL_NOTIFY_HOST" \
+  -u "$WERCKER_MD_MAIL_NOTIFY_USER" \
+  "$WERCKER_MD_NOTIFY_FILE"
