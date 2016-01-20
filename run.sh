5479045760bd5ef7760cd2ce10455731b6b7199b
Romeo Van Snick <romeovs@gmail.com>
2016-01-20 14:34:07 +0100

    add node version

--- run.sh
+++ run.sh
@@ -40,6 +40,9 @@ if [ "$WERCKER_MD_EMAIL_NOTIFY_ON" = "failed" ]; then
   fi
 fi
 
+echo "Sending email"
+node -v
+
 "$WERCKER_STEP_ROOT/node_modules/md-mailer/build/cli.js"
   -h "$WERCKER_MD_EMAIL_NOTIFY_HOST" \
   -u "$WERCKER_MD_EMAIL_NOTIFY_USER" \
