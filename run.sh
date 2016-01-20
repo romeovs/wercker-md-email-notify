3ea1b26ce0fce7677f1b8d136848be9c81fd7628
Romeo Van Snick <romeovs@gmail.com>
2016-01-20 11:54:54 +0100

    add support for port and on props and make a lot more robust

--- run.sh
+++ run.sh
@@ -1,12 +1,49 @@
 #!/bin/sh
 
+# check if required properties are set.
+if [ ! -n "$WERCKER_MD_EMAIL_NOTIFY_HOST" ]; then
+  error "Please specify \`host\` property"
+  exit 1
+fi
+
+if [ ! -n "$WERCKER_MD_EMAIL_NOTIFY_USER" ]; then
+  error "Please specify \`user\` property"
+  exit 1
+fi
+
+if [ ! -n "$WERCKER_MD_EMAIL_NOTIFY_PASS" ]; then
+  error "Please specify \`pass\` property"
+  exit 1
+fi
+
+if [ ! -n "$WERCKER_MD_EMAIL_NOTIFY_FILE" ]; then
+  error "Please specify \`file\` property"
+  exit 1
+fi
+
 # set the ssl option
-if [ "$WERCKER_MD_MAIL_NOTIFY_SSL" = "yes" ]; then
+if [ "$WERCKER_MD_EMAIL_NOTIFY_SSL" = "yes" ]; then
   SSL="--ssl"
 fi
 
-MDMAIL_PASS="$WERCKER_MD_MAILER_PASS" md-mailer
-  $SSL \
-  -h "$WERCKER_MD_MAIL_NOTIFY_HOST" \
-  -u "$WERCKER_MD_MAIL_NOTIFY_USER" \
-  "$WERCKER_MD_NOTIFY_FILE"
+# if port is set, use it
+if [ -n "$WERCKER_MD_EMAIL_NOTIFY_PORT" ]; then
+  PORT="-P"
+  PORTN="$WERCKER_MD_EMAIL_NOTIFY_PORT"
+fi
+
+# Skip if `on` is set to `failed` and build passed.
+if [ "$WERCKER_MD_EMAIL_NOTIFY_ON" = "failed" ]; then
+  if [ "$WERCKER_RESULT" = "passed" ]; then
+    echo "Build passed, skipping..."
+    return 0
+  fi
+fi
+
+md-mailer
+  -h "$WERCKER_MD_EMAIL_NOTIFY_HOST" \
+  -u "$WERCKER_MD_EMAIL_NOTIFY_USER" \
+  -p "$WERCKER_MD_EMAIL_NOTIFY_PASS" \
+      $SSL  \
+      $PORT "$PORTN" \
+     "$WERCKER_MD_EMAIL_NOTIFY_FILE"
